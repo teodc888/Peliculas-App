4dedef7049e7574fdb06d9d4cde42ff6b961239a
mateo <teodc4447@gmail.com>
2021-12-21 18:22:33 -0300

    probando

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,54 +1,62 @@
 import React from "react";
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import SearchBar from "../searchBar/searchBar";
 import CardMovies from "../cardMovies/cardMovies";
 import { useSelector } from "react-redux";
 import Stack from "@mui/material/Stack";
 export default function Home(props) {
-    const movies = useSelector(state => state.movies);
-     props.MTitulo("DELLACQUA MOVIES");
-    
+  const movies = useSelector((state) => state.movies);
+  props.MTitulo("DELLACQUA MOVIES");
+
   return (
     <div>
       <div>
-          <Stack
+        <Stack
           direction="column"
           alignItems="center"
           justifyContent="center"
           spacing={2}
-          >
-            <div className="Inicio">
+        >
+          <div className="Inicio">
             <h1>WELCOME</h1>
-            <h2>Millions of movies, TV shows and people to discover. Explore now.</h2>
+            <h2>
+              Millions of movies, TV shows and people to discover. Explore now.
+            </h2>
             <SearchBar />
-            </div>
-
-          </Stack>
+          </div>
+        </Stack>
       </div>
       <div>
-      <Box sx={{ width: '100%', marginTop:"50px" }}>
-      <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {
-              movies && movies.Search && movies.Search.map(movie => {
-                  return(
-                    <Grid item xs={4} sm={4} md={4} key={movie.imdbID} >
-                        <CardMovies 
-                           Title={movie.Title}
-                           Type={movie.Type}
-                           Year={movie.Year}
-                           Poster={movie.Poster}
-                           imdbID={movie.imdbID}
-                           numero={1}
-                        />
-                    </Grid>
-                  )
+        <Box sx={{ width: "100%", marginTop: "50px" }}>
+          <Grid
+            container
+            spacing={{ xs: 4, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            {movies && movies.Response === "False" ? (
+              <Grid item xs={12} sm={12} md={12} >
+                <img width="300" src="https://media2.giphy.com/media/YyKPbc5OOTSQE/giphy.gif?cid=ecf05e47rmbcqad11nefzwy06912qn1ao5rgt230zmwisl15&rid=giphy.gif&ct=g" alt="error" />
+              </Grid>
+            ) : (
+              movies.Search.map((movie) => {
+
+                return (
+                  <Grid item xs={4} sm={4} md={4} key={movie.imdbID}>
+                    <CardMovies
+                      Title={movie.Title}
+                      Type={movie.Type}
+                      Year={movie.Year}
+                      Poster={movie.Poster}
+                      imdbID={movie.imdbID}
+                      numero={1}
+                    />
+                  </Grid>
+                );
               })
-          }
-
-
-      </Grid>
-    </Box>
+            )}
+          </Grid>
+        </Box>
       </div>
     </div>
   );
